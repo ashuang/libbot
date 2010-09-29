d78b2b454029871405f3f2a9e4ecad80dd6d0ac9
abachrac <abachrac@9146db76-5286-039a-5f50-05d86fea6eed>
2010-09-29 00:18:08 +0000

    reverting back... not sure why this got changed....

--- pods/bot2-lcmgl/python/src/bot_lcmgl/__init__.py
+++ pods/bot2-lcmgl/python/src/bot_lcmgl/__init__.py
@@ -1,11 +1,3 @@
 from data_t import data_t
 
 from lcmgl import *
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
-from data_t import data_t
