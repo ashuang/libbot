dd8130fe63d9f5fcf86b12bb3c4e7823e929c76f
abachrac <abachrac@9146db76-5286-039a-5f50-05d86fea6eed>
2011-02-24 01:12:32 +0000

    add the circbuf structure/functions to serial.c/h

--- bot2-core/src/bot_core/serial.c
+++ bot2-core/src/bot_core/serial.c
@@ -2,6 +2,10 @@
     serial class, we let you use the fd (or FILE*) directly.
 
     eolson@mit.edu, 2004
+
+    ---------
+    added the circbuf circulur buffer utilities
+    abachrac@mit.edu, 2011
 **/
 
 #include <sys/types.h>
@@ -11,7 +15,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <string.h>
 #include <stdlib.h>
 
 #ifdef __linux__
@@ -23,6 +27,9 @@
 
 #include "serial.h"
 
+#ifndef MIN
+#define MIN(a,b)((a < b) ? a : b)
+#endif
 
 static int bot_serial_translate_baud(int inrate);
 
@@ -278,3 +285,156 @@ int bot_serial_close(int fd)
 {
 	return close(fd);
 }
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+struct _BotSerialCircBuf{
+        char * buf;
+        int readOffset;
+        int writeOffset;
+        int numBytes;
+        int maxSize;
+};
+
+BotSerialCircBuf * bot_serial_circbuf_create(int size)
+{
+  //create buffer, and allocate space for size bytes
+  BotSerialCircBuf * cbuf = (BotSerialCircBuf *) calloc(1,sizeof(BotSerialCircBuf));
+  cbuf->buf = (char *) malloc(size * sizeof(char));
+  cbuf->readOffset = cbuf->writeOffset = cbuf->numBytes = 0;
+  cbuf->maxSize = size;
+  return cbuf;
+}
+
+void bot_serial_circbuf_destroy(BotSerialCircBuf * cbuf)
+{
+  //destroy
+  if (cbuf->buf!=NULL)
+    free(cbuf->buf);
+  free(cbuf);
+}
+
+int bot_serial_circbuf_read(BotSerialCircBuf * cbuf, int numBytes, char * buf)
+{
+  //read numBytes
+  int bytes_read = bot_serial_circbuf_peek(cbuf, numBytes, buf);
+
+  if (bytes_read > 0){
+    //move readPtr
+    cbuf->numBytes -= bytes_read;
+    cbuf->readOffset = (cbuf->readOffset + bytes_read) % cbuf->maxSize;
+  }
+
+  return bytes_read;
+
+}
+
+int bot_serial_circbuf_write(BotSerialCircBuf * cbuf, int numBytes, char * buf)
+{
+
+  //check if there is enough space... maybe this should just wrap around??
+  if (numBytes + cbuf->numBytes > cbuf->maxSize) {
+    fprintf(stderr, "CIRC_BUF ERROR: not enough space in circular buffer,Discarding data!\n");
+    numBytes = cbuf->maxSize - cbuf->numBytes;
+
+  }
+  //write to wrap around point.
+  int bytes_written = MIN(cbuf->maxSize - cbuf->writeOffset, numBytes);
+  memcpy(cbuf->buf + cbuf->writeOffset, buf, bytes_written * sizeof(char));
+  numBytes -= bytes_written;
+
+  //write the rest from start of buffer
+  if (numBytes > 0) {
+    memcpy(cbuf->buf, buf + bytes_written, numBytes * sizeof(char));
+    bytes_written += numBytes;
+  }
+
+  //move writePtr
+  cbuf->numBytes += bytes_written;
+  cbuf->writeOffset = (cbuf->writeOffset + bytes_written) % cbuf->maxSize;
+
+  return bytes_written;
+
+}
+
+int bot_serial_circbuf_peek(BotSerialCircBuf * cbuf, int numBytes, char * buf)
+{
+  //read numBytes from start of buffer, but don't move readPtr
+  if (numBytes > cbuf->numBytes || numBytes > cbuf->maxSize) {
+    fprintf(stderr, "CIRC_BUF ERROR: Can't read %d bytes from the circular buffer, only %d available! \n",
+        numBytes,cbuf->numBytes);
+    return -1;
+  }
+  //read up to wrap around point
+  int bytes_read = MIN(cbuf->maxSize - cbuf->readOffset, numBytes);
+  memcpy(buf, cbuf->buf + cbuf->readOffset, bytes_read * sizeof(char));
+  numBytes -= bytes_read;
+
+  //read again from beginning if there are bytes left
+  if (numBytes > 0) {
+    memcpy(buf + bytes_read, cbuf->buf, numBytes * sizeof(char));
+    bytes_read += numBytes;
+  }
+  return bytes_read;
+}
+
+int bot_serial_circbuf_flush(BotSerialCircBuf * cbuf)
+{
+  //move pointers to "empty" the read buffer
+  cbuf->readOffset = cbuf->writeOffset = cbuf->numBytes = 0;
+  return 0;
+
+}
+
+int bot_serial_circbuf_available(BotSerialCircBuf * cbuf) {
+        return cbuf->numBytes;
+}
+
+#if 0
+void bot_serial_circbuf_unit_test()
+{
+  char * testString = "iuerrlfkladbytes_writtenbytes_writte";
+  char comp[1000];
+  char * comp_p = comp;
+  BotSerialCircBuf cbuf;
+  bot_serial_circbuf_create(&cbuf, 13);
+
+  int numWritten = 0;
+  int writeAmount = 0;
+  int numRead = 0;
+  int readAmount = 0;
+
+  writeAmount = 4;
+  bot_serial_circbuf_write(&cbuf, writeAmount, testString + numWritten);
+  numWritten += writeAmount;
+
+  writeAmount = 6;
+  bot_serial_circbuf_write(&cbuf, writeAmount, testString + numWritten);
+  numWritten += writeAmount;
+
+  readAmount = 9;
+  bot_serial_circbuf_read(&cbuf, readAmount, comp_p + numRead);
+  numRead += readAmount;
+
+  writeAmount = 11;
+  bot_serial_circbuf_write(&cbuf, writeAmount, testString + numWritten);
+  numWritten += writeAmount;
+
+  readAmount = 12;
+  bot_serial_circbuf_read(&cbuf, readAmount, comp_p + numRead);
+  numRead += readAmount;
+
+  printf("at end, there are %d bytes left, should be %d \n", cbuf.numBytes, numWritten - numRead);
+
+  if (strncmp(testString, comp, numRead) == 0)
+    printf("WOOOHOO! the strings match :-)\n");
+  else
+    printf("BOOOO Somethings wrong");
+
+  bot_serial_circbuf_destroy(&cbuf);
+}
+#endif
