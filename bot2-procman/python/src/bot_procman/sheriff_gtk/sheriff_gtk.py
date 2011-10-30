12ff67dff12210bb5af0ea36a040ce7b70a7e035
albert <albert@9146db76-5286-039a-5f50-05d86fea6eed>
2011-10-30 19:28:23 +0000

    refactor: split SheriffHostModel, SheriffHostTreeView off into hosts_treeview.py

--- bot2-procman/python/src/bot_procman/sheriff_gtk/sheriff_gtk.py
+++ bot2-procman/python/src/bot_procman/sheriff_gtk/sheriff_gtk.py
@@ -24,6 +24,7 @@ import bot_procman.sheriff_gtk.command_model as cm
 import bot_procman.sheriff_gtk.command_treeview as ctv
 import bot_procman.sheriff_gtk.sheriff_dialogs as sd
 import bot_procman.sheriff_gtk.command_console as cc
+import bot_procman.sheriff_gtk.hosts_treeview as ht
 
 try:
     from bot_procman.build_prefix import BUILD_PREFIX
@@ -40,122 +41,6 @@ def find_bot_procman_deputy_cmd():
         if os.path.exists(fname) and os.path.isfile(fname):
             return fname
     return None
-
-class SheriffHostModel(gtk.ListStore):
-    COL_OBJ, \
-    COL_NAME, \
-    COL_LAST_UPDATE, \
-    COL_LOAD, \
-    NUM_ROWS = range(5)
-
-    def __init__(self, _sheriff):
-        super(SheriffHostModel, self).__init__(gobject.TYPE_PYOBJECT,
-                gobject.TYPE_STRING, # deputy name
-                gobject.TYPE_STRING, # last update time
-                gobject.TYPE_STRING, # load
-                )
-        self.sheriff = _sheriff
-
-    def update(self):
-        to_update = set(self.sheriff.get_deputies ())
-        to_remove = []
-
-        def _deputy_last_update_str (dep):
-            if dep.last_update_utime:
-                now_utime = time.time () * 1000000
-                return "%.1f seconds ago" % ((now_utime-dep.last_update_utime) * 1e-6)
-            else:
-                return "<never>"
-
-        def _update_host_row (model, path, model_iter, user_data):
-            deputy = model.get_value (model_iter, SheriffHostModel.COL_OBJ)
-            if deputy in to_update:
-                model.set (model_iter, 
-                        SheriffHostModel.COL_LAST_UPDATE, 
-                        _deputy_last_update_str (deputy),
-                        SheriffHostModel.COL_LOAD, 
-                        "%f" % deputy.cpu_load,
-                        )
-                to_update.remove (deputy)
-            else:
-                to_remove.append (gtk.TreeRowReference (model, path))
-
-        self.foreach (_update_host_row, None)
-
-        for trr in to_remove:
-            self.remove (self.get_iter (trr.get_path()))
-
-        for deputy in to_update:
-            print "adding %s to treeview" % deputy.name
-            new_row = (deputy, deputy.name, _deputy_last_update_str (deputy),
-                    "%f" % deputy.cpu_load,
-                    )
-            self.append (new_row)
-
-
-class SheriffHostTreeView(gtk.TreeView):
-    def __init__(self, _sheriff, hosts_ts):
-        super(SheriffHostTreeView, self).__init__(hosts_ts)
-        self.sheriff = _sheriff
-        self.hosts_ts = hosts_ts
-
-        plain_tr = gtk.CellRendererText ()
-        col = gtk.TreeViewColumn ("Host", plain_tr, text=SheriffHostModel.COL_NAME)
-        col.set_sort_column_id (1)
-        col.set_resizable (True)
-        self.append_column (col)
-
-        last_update_tr = gtk.CellRendererText()
-        col = gtk.TreeViewColumn ("Last update", last_update_tr, text=SheriffHostModel.COL_LAST_UPDATE)
-#        col.set_sort_column_id (2) # XXX this triggers really weird bugs...
-        col.set_resizable (True)
-        col.set_cell_data_func(last_update_tr, self._deputy_last_update_cell_data_func)
-        self.append_column (col)
-
-        col = gtk.TreeViewColumn ("Load", plain_tr, text=SheriffHostModel.COL_LOAD)
-        col.set_resizable (True)
-        self.append_column (col)
-
-        self.connect ("button-press-event",
-                self._on_hosts_tv_button_press_event)
-
-        # hosts treeview context menu
-        self.hosts_ctxt_menu = gtk.Menu ()
-
-        self.cleanup_hosts_ctxt_mi = gtk.MenuItem ("_Cleanup")
-        self.hosts_ctxt_menu.append (self.cleanup_hosts_ctxt_mi)
-        self.cleanup_hosts_ctxt_mi.connect ("activate", 
-                self._cleanup_hosts)
-        self.hosts_ctxt_menu.show_all()
-
-    def _on_hosts_tv_button_press_event (self, treeview, event):
-        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
-            self.hosts_ctxt_menu.popup (None, None, None, event.button, event.time)
-            return True
-
-    def _cleanup_hosts(self, *args):
-        self.sheriff.purge_useless_deputies()
-        self.hosts_ts.update()
-
-    def _deputy_last_update_cell_data_func (self, column, cell, model, model_iter):
-        # bit of a hack to pull out the last update time
-        try:
-            last_update = float(model.get_value(model_iter, SheriffHostModel.COL_LAST_UPDATE).split()[0])
-        except:
-            last_update = None
-        if last_update is None or last_update > 5:
-            cell.set_property("cell-background-set", True)
-            cell.set_property("cell-background", "Red")
-        elif last_update > 2:
-            cell.set_property("cell-background-set", True)
-            cell.set_property("cell-background", "Yellow")
-        else:
-            cell.set_property("cell-background-set", False)
-
-#    def _get_selected_hosts (self):
-#        model, rows = self.hosts_tv.get_selection ().get_selected_rows ()
-#        return [ model.get_value (model.get_iter(path), 0) \
-#                for path in rows ]
 
 class SheriffGtkConfig(object):
     def __init__(self):
@@ -412,8 +297,9 @@ class SheriffGtk(object):
             col_cmi.connect ("activate", on_activate, col)
             view_menu.append (col_cmi)
 
-        self.hosts_ts = SheriffHostModel(self.sheriff)
-        self.hosts_tv = SheriffHostTreeView(self.sheriff, self.hosts_ts)
+        # setup the hosts treeview
+        self.hosts_ts = ht.SheriffHostModel(self.sheriff)
+        self.hosts_tv = ht.SheriffHostTreeView(self.sheriff, self.hosts_ts)
         sw = gtk.ScrolledWindow ()
         sw.set_policy (gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         hpane.pack2 (sw, resize = False)
