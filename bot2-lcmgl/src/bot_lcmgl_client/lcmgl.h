f12cd13ad6c79b80f6d8f83f56d701f8ab319564
abachrac <abachrac@9146db76-5286-039a-5f50-05d86fea6eed>
2011-11-10 15:18:08 +0000

    add bot_lcmgl_vertex2dv and bot_lcmgl_vertex2fv

--- bot2-lcmgl/src/bot_lcmgl_client/lcmgl.h
+++ bot2-lcmgl/src/bot_lcmgl_client/lcmgl.h
@@ -94,6 +94,10 @@ void bot_lcmgl_depth_func(bot_lcmgl_t *lcmgl, unsigned int func);
 
 
 //vector versions of some of the above
+static inline void bot_lcmgl_vertex2fv(bot_lcmgl_t * lcmgl, const float v[2]){bot_lcmgl_vertex2f(lcmgl,v[0],v[1]);}
+static inline void bot_lcmgl_vertex2dv(bot_lcmgl_t * lcmgl, const double v[2]){bot_lcmgl_vertex2d(lcmgl,v[0],v[1]);}
+
+
 static inline void bot_lcmgl_vertex3fv(bot_lcmgl_t * lcmgl, const float v[3]){bot_lcmgl_vertex3f(lcmgl,v[0],v[1],v[2]);}
 static inline void bot_lcmgl_vertex3dv(bot_lcmgl_t * lcmgl, const double v[3]){bot_lcmgl_vertex3d(lcmgl,v[0],v[1],v[2]);}
 
